40b1dec19b6218d4360afe07fabfb212b0c4baf5
Andy Shearer <andyshearer.95@googlemail.com>
2022-05-04 11:47:18 +0100

    Add send SMS functionality, and listing msgs in conversation
    I'm passing in the CREATED_CONVERSATION_SID, SENDER_NUMBER, and VERIFIED_RECIPIENT_NUMBER through environment variables. The CREATED_CONVERSATION_SID can be looked up, but is easier to hardcode whilst I'm testing.

--- scripts/SMSHandler.js
+++ scripts/SMSHandler.js
@@ -1,13 +1,42 @@
 require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+const verifiedRcp = process.env.VERIFIED_RECIPIENT_NUMBER;
+const senderNo = process.env.SENDER_NUMBER;
+const convSid = process.env.CREATED_CONVERSATION_SID;   // CREATED_CONVERSATION_SID is the conversation I've already created between these two numbers
+const client = require('twilio')(accountSid, authToken);
+
+loadMessagesFor = async (conversationSid, msgLimit = 20) => {
+  console.log("Fetching up to 20 messages for conversation", conversationSid);
+  await client.conversations.conversations(conversationSid)
+        .messages
+        .list({msgLimit})
+        .then(messages => messages.forEach(m => console.log("Message SID:", m.sid, "Message Body:", m.body)));
+};
+
+sendMessage = async (conversationSid, message) => {
+  await client.conversations.conversations(conversationSid)
+        .messages
+        .create({author: 'moodBot', body: message})
+        .then(message => console.debug("Sent a new message with SID", message.sid));
+};
 
 async function main() {
-  console.log(accountSid, authToken);
-  const client = require('twilio')(accountSid, authToken);
-  client.conversations.conversations
-        .create({friendlyName: 'AndyS First Conversation'})
-        .then(conversation => console.log(conversation.sid));
+//  const conversation = await client.conversations.conversations
+//    .create({friendlyName: 'AndyS New Conversation #1'});
+
+//  console.log(conversation);
+//  const convSid = conversation.sid;
+//  await client.conversations.conversations(convSid)
+//    .participants
+//    .create({
+//       'messagingBinding.address': verifiedRcp,
+//       'messagingBinding.proxyAddress': senderNo
+//     })
+//    .then(participant => console.log(participant.sid));
+  const timeString = new Date().toLocaleTimeString("en-GB");
+  await sendMessage(convSid, `Hey this message is going out to you at ${timeString}`);
+  await loadMessagesFor(convSid);
 }
 
 main()
