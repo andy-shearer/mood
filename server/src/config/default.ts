7f41c45bb5f4b7354b56d206e4c1b76b50c0ec4d
ErnestCodes <emmycontacts@gmail.com>
2022-05-05 22:48:39 +0100

    connect to database

---
+++ server/src/config/default.ts
@@ -0,0 +1,14 @@
+export default {
+  port: 1337,
+  dbUri:
+    "mongodb+srv://emmy123:emmy123@devnetwork.r0rje.mongodb.net/mood?retryWrites=true&w=majority",
+  saltWorkFactor: 10,
+  accessTokenTtl: "15m",
+  refreshTokenTtl: "1y",
+  //   privateKey:
+  //   publicKey:
+  //   accessTokenPrivateKey: ``,
+  //   accessTokenPublicKey: ``,
+  //   refreshTokenPrivateKey: ``,
+  //   refreshTokenPublicKey: ``,
+};
---
+++ server/src/utils/connect.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "config";
+import logger from "./logger";
+
+async function connect() {
+  const dbUri = config.get<string>("dbUri");
+  try {
+    await mongoose.connect(dbUri);
+    logger.info("Database connected");
+  } catch (error) {
+    logger.error("Could not connect to db");
+    //  for when unhandled fatal exceptions occur
+    //  that was not handled by the domain
+    process.exit(1);
+  }
+}
+
+export default connect;
