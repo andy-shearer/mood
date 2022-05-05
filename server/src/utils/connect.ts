ccb1e304a2198c20eab3c38ac006f100cf818573
ErnestCodes <emmycontacts@gmail.com>
2022-05-06 00:49:35 +0100

    fix bug with tsconfig

--- server/src/utils/connect.ts
+++ server/src/utils/connect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import config from "config";
 import logger from "./logger";
+import config from "config";
 
 async function connect() {
   const dbUri = config.get<string>("dbUri");
