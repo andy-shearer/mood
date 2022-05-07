d64fc3c224544cad19b44b0edb94f284e6751659
ErnestCodes <emmycontacts@gmail.com>
2022-05-07 13:07:43 +0100

    Implement OAuth on server

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,14 @@
 import { Response, Request, Express } from "express";
+import { googleOauthHandler } from "./controllers/session.controller";
 
 function routes(app: Express) {
   // Performance & healthCheck
   app.get("/healthCheck", (req: Request, res: Response) => {
     res.sendStatus(200);
   });
+
+  // OAuth
+  app.get("/api/sessions/oauth/google", googleOauthHandler);
 }
 
 export default routes;
---
+++ server/src/utils/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+// import deserializeUser from "../middleware/deserializeUser";
+import routes from "../routes";
+
+function creatServer() {
+  const app = express();
+
+  app.use(express.json());
+
+  //   app.use(deserializeUser);
+
+  routes(app);
+
+  return app;
+}
+
+export default creatServer;
