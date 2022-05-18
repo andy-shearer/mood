8d36a44b2a16bf0cd2ddf9e04ea51022b30b38f9
Andy Shearer <andyshearer.95@googlemail.com>
2022-05-18 14:53:30 +0100

    Tidy up logging for inbound messages

--- frontend/pages/api/webhook.js
+++ frontend/pages/api/webhook.js
@@ -2,17 +2,33 @@
 export default function handler(req, res) {
   // TODO: Authenticate 'X-Twilio-Signature' header
   res.status(200).json({
-    message: 'Received!'
+    message: 'Hi 👋'
   });
 
-  // Just debug the inbound request currently
   console.log("\n====================================\n", new Date().toLocaleTimeString("en-GB"));
+  if(req.body.EventType === "onMessageAdded") {
+    console.log("RECEIVED INBOUND MESSAGE from", req.body.Author);
+    console.log(req.body);
+  }
 
-  console.log(req.method, req.url, 'HTTP/' + req.httpVersion);
-//  for (var name in req.headers) {
-//    console.log(name + ':', req.headers[name]);
-//  }
-  console.log(req.body);
   //console.debug(req);
   console.log("\n====================================\n");
 }
+
+// EXAMPLE 'onMessageAdded' event HTTP POST body:
+//
+//{
+//  MessagingServiceSid: 'MGXXX...',
+//  EventType: 'onMessageAdded',
+//  Attributes: '{}',
+//  DateCreated: '2022-05-18T13:42:17.312Z',
+//  Index: '4',
+//  MessageSid: 'IMXXX...',
+//  AccountSid: 'ACXXX...',
+//  Source: 'SMS',
+//  RetryCount: '0',
+//  Author: '+447...',
+//  ParticipantSid: 'MBXXX...',
+//  Body: 'This is the actual message content',
+//  ConversationSid: 'CHXXX...'
+//}
