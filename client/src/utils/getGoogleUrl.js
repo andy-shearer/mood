18db9a6eb26310c94df5ac5a2e5bb1ec066b5ed0
Andy Shearer <andyshearer.95@googlemail.com>
2022-05-27 21:07:41 +0100

    Fix OAuth

--- client/src/utils/getGoogleUrl.js
+++ client/src/utils/getGoogleUrl.js
@@ -2,8 +2,8 @@ function getGoogleOAuthURL() {
   const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 
   const options = {
-    redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URL,
-    client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    redirect_uri: "https://mood-nft.herokuapp.com/api/sessions/oauth/google",
+    client_id: "652470402054-qnlbt8p80d67b7kg236540rj3s8bsrdo.apps.googleusercontent.com",
     access_type: "offline",
     response_type: "code",
     prompt: "consent",
